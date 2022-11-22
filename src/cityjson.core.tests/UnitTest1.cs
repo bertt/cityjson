3b560f7924bd3b75e0364667c15a27e9f9416a26
bertt <btemme@hotmail.com>
2022-11-22 15:34:23 +0100

    add another test dataset

--- src/cityjson.core.tests/UnitTest1.cs
+++ src/cityjson.core.tests/UnitTest1.cs
@@ -17,5 +17,16 @@ namespace CityJSON.Tests
             var expected = new List<double>() { 78248.66, 457604.591, 2.463, 79036.024, 458276.439, 37.481 };
             Assert.IsTrue(cityjson.Metadata.GeographicalExtent.Length == expected.ToArray().Length);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var json = File.ReadAllText("fixtures/25gn1_04_2020_gebouwen.json");
+            var cityjson = JsonConvert.DeserializeObject<CityJSONRoot>(json);
+            Assert.IsTrue(cityjson.Version == "1.0");
+            var expected = new List<double>() { 78248.66, 457604.591, 2.463, 79036.024, 458276.439, 37.481 };
+            Assert.IsTrue(cityjson.Metadata.GeographicalExtent.Length == expected.ToArray().Length);
+        }
+
     }
 }
