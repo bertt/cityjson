3e4a6d5edf3a1063dbd7632fbe334940cccb28f5
Bert Temme <btemme@hotmail.com>
2024-12-04 16:32:17 +0100

    cleaning + to .net 8

--- src/cityjson.core.tests/UnitTest1.cs
+++ src/cityjson.core.tests/UnitTest1.cs
@@ -1,28 +1,26 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
 using System.IO;
+using NUnit.Framework;
 
 namespace CityJSON.Tests
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        [Test]
         public void TestMethod1()
         {
             var json = File.ReadAllText("fixtures/denhaag.json");
             var cityjson = CityJsonRoot.FromJson(json);
-            Assert.IsTrue(cityjson.Version == "1.0");
-            Assert.IsTrue(cityjson.CityObjects.Count == 2498);
+            Assert.That(cityjson.Version == "1.0");
+            Assert.That(cityjson.CityObjects.Count == 2498);
         }
 
-        [TestMethod]
+        [Test]
         public void TestMethod2()
         {
             var json = File.ReadAllText("fixtures/25gn1_04_2020_gebouwen.json");
             var cityjson = CityJsonRoot.FromJson(json);
-            Assert.IsTrue(cityjson.Version == "1.0");
-            Assert.IsTrue(cityjson.CityObjects.Count == 7313);
+            Assert.That(cityjson.Version == "1.0");
+            Assert.That(cityjson.CityObjects.Count == 7313);
         }
     }
 }
