f7469d7b7ec907400090da65343ea02ec2b91b02
Bert Temme <btemme@hotmail.com>
2024-12-04 17:17:14 +0100

    add reading minimal 2.0.1 test

--- src/cityjson.core.tests/UnitTest1.cs
+++ src/cityjson.core.tests/UnitTest1.cs
@@ -6,6 +6,31 @@ namespace CityJSON.Tests
     public class UnitTest1
     {
         [Test]
+        public void ReadMinimal20CityJson()
+        {
+            var json = File.ReadAllText("fixtures/minimal.city.json");
+            var cityjson = CityJsonRoot.FromJson(json);
+
+            Assert.That(cityjson.Type == CityJsonRootType.CityJson);
+            Assert.That(cityjson.Version == "2.0");
+            Assert.That(cityjson.CityObjects.Count == 0);
+
+            Assert.That(cityjson.Transform.Scale.Length == 3);
+            Assert.That(cityjson.Transform.Scale[0] == 1);
+            Assert.That(cityjson.Transform.Scale[1] == 1);
+            Assert.That(cityjson.Transform.Scale[2] == 1);
+
+            Assert.That(cityjson.Transform.Translate.Length == 3);
+            Assert.That(cityjson.Transform.Translate[0] == 0);
+            Assert.That(cityjson.Transform.Translate[1] == 0);
+            Assert.That(cityjson.Transform.Translate[2] == 0);
+
+            Assert.That(cityjson.CityObjects.Count == 0);
+            Assert.That(cityjson.Vertices.Length== 0);
+        }
+
+
+        [Test]
         public void TestMethod1()
         {
             var json = File.ReadAllText("fixtures/denhaag.json");
