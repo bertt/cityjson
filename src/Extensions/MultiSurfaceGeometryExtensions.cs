da704cfe57fa8d19416f65bf58e825835d5fb3b0
Bert Temme <btemme@hotmail.com>
2024-12-16 13:27:26 +0100

    reorder2

---
+++ src/Extensions/MultiSurfaceGeometryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using CityJSON.Geometry;
+using CityJSON.IO;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Noding;
+
+namespace CityJSON.Extensions
+{
+    public static class MultiSurfaceGeometryExtensions
+    {
+        public static List<Polygon> ToPolys(this MultiSurfaceGeometry multiSurfaceGeometry, List<Vertex> vertices, Transform transform)
+        {
+            var polygons = new List<Polygon>();
+
+            foreach(var bnd in multiSurfaceGeometry.Boundaries)
+            {
+                var outer = bnd[0];
+
+                var holes = bnd.Length>1?bnd[1..]:null;
+
+                var poly = PolygonCreator.GetPolygon(vertices, outer, transform, holes);
+                polygons.Add(poly);
+            }
+
+            return polygons;
+        }
+    }
+}
---
+++ src/IO/PolygonCreator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using CityJSON.Extensions;
+using NetTopologySuite.Geometries;
+
+namespace CityJSON.IO
+{
+    public static class PolygonCreator
+    {
+        public static Polygon GetPolygon(List<Vertex> vertices, int[] vertexList, Transform transform, int[][] holes = null)
+        {
+            var coords = new Coordinate[vertexList.Length + 1];
+            for (int i = 0; i < vertexList.Length; i++)
+            {
+                var vertex = vertices[vertexList[i]];
+                var coordinate = new CoordinateZ(vertex.X, vertex.Y, vertex.Z);
+
+                coords[i] = coordinate.Transform(transform);
+            }
+            coords[vertexList.Length] = coords[0];
+            var geometryFactory = new GeometryFactory();
+            var extRing = geometryFactory.CreateLinearRing(coords);
+            Polygon polygon = null;
+            if (holes != null)
+            {
+                foreach (var hole in holes)
+                {
+                    var holeCoords = new Coordinate[hole.Length + 1];
+                    for (int i = 0; i < hole.Length; i++)
+                    {
+                        var vertex = vertices[hole[i]];
+                        var coordinate = new CoordinateZ(vertex.X, vertex.Y, vertex.Z);
+                        holeCoords[i] = coordinate.Transform(transform);
+                    }
+                    holeCoords[hole.Length] = holeCoords[0];
+                    var holeRing = geometryFactory.CreateLinearRing(holeCoords);
+                    polygon = geometryFactory.CreatePolygon((LinearRing)extRing, new LinearRing[] { holeRing });
+                }
+            }
+            else
+            {
+                polygon = geometryFactory.CreatePolygon(extRing);
+            }
+            return polygon;
+        }
+
+    }
+}
