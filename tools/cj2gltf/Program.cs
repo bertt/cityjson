be792f6937a6c00299716df83efcb307d286eef4
Bert Temme <btemme@hotmail.com>
2025-04-02 11:44:16 +0200

    add a start of cj2gltf

--- tools/cj2gltf/Program.cs
+++ tools/cj2gltf/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.CommandLine;
+
+var inputFile = new Argument<string>("input CityJSON file");
+var outputFile = new Argument<string>("Output glTF 2.0 file");
+
+var rootCommand = new RootCommand("CLI tool for converting from CityJSON to glTF 2.0")
+{
+    inputFile, outputFile
+};
+
+rootCommand.SetHandler(RunAsync, inputFile, outputFile);
+
+await rootCommand.InvokeAsync(args);
+
+async Task RunAsync(string inputFile, string outputFile)
+{
+    Console.WriteLine("cj2gltf 0.1");
+}
